fb3e4d7d3a4666ba70fac9ddf31751234e7fa46d
olexiy86 <157749524+olexiy86@users.noreply.github.com>
2024-03-19 11:12:58 -0400

    US_household_income_Data_Cleaning.sql

--- US_household_income_DA.sql
+++ US_household_income_DA.sql
@@ -1,6 +1,6 @@
 /* 
 
-US Household Income 2022
+US Household Income 
 (Data Cleaning)
 
 */
@@ -26,7 +26,8 @@ FROM US_project.us_household_income_statistics;
 
 -- Identifying duplicates and deleting them
 
-SELECT id, COUNT(id)
+SELECT  id, 
+	COUNT(id)
 FROM US_project.us_household_income
 GROUP BY id
 HAVING COUNT(id) > 1
@@ -35,8 +36,9 @@ HAVING COUNT(id) > 1
 
 SELECT *
 FROM (
-SELECT row_id, id,
-		ROW_NUMBER() OVER(PARTITION BY id ORDER BY id) row_num
+SELECT row_id, 
+	id,
+	ROW_NUMBER() OVER(PARTITION BY id ORDER BY id) row_num
 		FROM us_household_income) duplicates
 WHERE row_num > 1
 ;
@@ -45,8 +47,9 @@ DELETE FROM us_household_income
 WHERE row_id IN (
 	SELECT row_id
 	FROM (
-		SELECT row_id, id,
-				ROW_NUMBER() OVER(PARTITION BY id ORDER BY id) row_num
+		SELECT  row_id, 
+			id,
+			ROW_NUMBER() OVER(PARTITION BY id ORDER BY id) row_num
 		FROM us_household_income) duplicates
 	WHERE row_num > 1)
 ;
@@ -71,7 +74,9 @@ SET State_Name = 'Alabama'
 WHERE State_Name = 'alabama'
 ;
 
+
 -- Correcting inconsistent values
+
 SELECT *
 FROM US_project.us_household_income
 WHERE County = 'Autauga County'
@@ -108,6 +113,7 @@ WHERE AWater = 0 OR AWater = ' ' OR AWater IS NULL
 
 
 -- Some irregular values where some counties have no land area
+
 SELECT County, ALand, AWater
 FROM US_project.us_household_income
 WHERE (ALand = 0 OR ALand = ' ' OR ALand IS NULL)
