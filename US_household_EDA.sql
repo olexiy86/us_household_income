9a6de5187387e173aa4260084c0847205198011f
olexiy86 <157749524+olexiy86@users.noreply.github.com>
2024-03-19 11:46:45 -0400

    Update US_household_EDA.sql

--- US_household_EDA.sql
+++ US_household_EDA.sql
@@ -16,9 +16,9 @@ FROM US_project.us_household_income_statistics;
 
 -- Finding total area of land and water of each state
 
-SELECT State_Name,
-		SUM(ALand), 
-		SUM(AWater)
+SELECT  State_Name,
+	SUM(ALand), 
+	SUM(AWater)
 FROM US_project.us_household_income
 GROUP BY State_Name
 ORDER BY 3 DESC
@@ -35,7 +35,11 @@ WHERE Mean <> 0
 ;
    
 
-SELECT u.State_Name, County, Type, 'Primary', Mean, Median
+SELECT u.State_Name, 
+	County, Type, 
+	'Primary', 
+	Mean, 
+	Median
 FROM US_project.us_household_income  u
 INNER JOIN US_project.us_household_income_statistics us
 	ON u.id = us.id
@@ -43,8 +47,9 @@ WHERE Mean <> 0
 ;
 
 -- TOP 5 lowest income states
-SELECT u.State_Name, 
-		ROUND(AVG(Mean), 1), 
+
+SELECT  u.State_Name, 
+	ROUND(AVG(Mean), 1), 
         ROUND(AVG(Median), 1)
 FROM US_project.us_household_income  u
 INNER JOIN US_project.us_household_income_statistics us
@@ -57,8 +62,9 @@ LIMIT 5
 
 
 -- TOP 10 highest income states
-SELECT u.State_Name, 
-		ROUND(AVG(Mean), 1) AS avg_salary, 
+
+SELECT  u.State_Name, 
+	ROUND(AVG(Mean), 1) AS avg_salary, 
         ROUND(AVG(Median), 1) AS median_salary
 FROM US_project.us_household_income  u
 INNER JOIN US_project.us_household_income_statistics us
@@ -72,21 +78,24 @@ LIMIT 10
 
 -- Income by Type of locality 
 
-SELECT Type, COUNT(Type),
-		ROUND(AVG(Mean), 1), 
+SELECT  Type, 
+	COUNT(Type),
+	ROUND(AVG(Mean), 1), 
         ROUND(AVG(Median), 1)
 FROM US_project.us_household_income  u
 INNER JOIN US_project.us_household_income_statistics us
 	ON u.id = us.id
 WHERE Mean <> 0
 GROUP BY 1
-
--- Excluding outliers by:
+	
+-- excluding outliers by:
+	
 HAVING COUNT(Type) > 100
 -- end 
 ORDER BY 4 DESC
 LIMIT 20
 ;
+
 
 
 -- Low income states with County Urban and Community
@@ -96,10 +105,11 @@ FROM US_project.us_household_income
 WHERE Type = 'Community'
 ;
 
+
 -- Finding cities with the highest household income
 
-SELECT u.State_Name, City, 
-		ROUND(AVG(Mean), 1) AS avg_income,
+SELECT  u.State_Name, City, 
+	ROUND(AVG(Mean), 1) AS avg_income,
         ROUND(AVG(Median), 1) AS median_income
 FROM US_project.us_household_income  u
 RIGHT JOIN US_project.us_household_income_statistics us
